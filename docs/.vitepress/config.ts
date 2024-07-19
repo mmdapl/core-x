e439a049ee84afbb5bee034788d779802ecbb8df
chufan <mmdapl@163.com>
2024-07-19 11:38:56 +0800

    docs: 修改文档模块，支持项目包启动

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,4 @@
+import { i18n } from '@142vip/vitepress'
 import { defineConfig } from 'vitepress'
 import { navbar } from './config/navbar'
 import { sidebar } from './config/sidebar'
@@ -5,39 +6,6 @@ import { footer } from './config/footer'
 
 // https://vitepress.dev/reference/site-config
 
-const i18n: ThemeConfig['i18n'] = {
-  search: '搜索',
-  menu: '菜单',
-  toc: '本页目录',
-  returnToTop: '返回顶部',
-  appearance: '外观',
-  previous: '上一篇',
-  next: '下一篇',
-  pageNotFound: '页面未找到',
-  deadLink: {
-    before: '你打开了一个不存在的链接：',
-    after: '。',
-  },
-  deadLinkReport: {
-    before: '不介意的话请提交到',
-    link: '这里',
-    after: '，我们会跟进修复。',
-  },
-  footerLicense: {
-    before: '',
-    after: '',
-  },
-  ariaAnnouncer: {
-    before: '',
-    after: '已经加载完毕',
-  },
-  ariaDarkMode: '切换深色模式',
-  ariaSkipToContent: '直接跳到内容',
-  ariaToC: '当前页面的目录',
-  ariaMainNav: '主导航',
-  ariaMobileNav: '移动版导航',
-  ariaSidebarNav: '侧边栏导航',
-}
 export default defineConfig({
   lang: 'zh-CN',
   title: '@142vip/core-x',
@@ -71,7 +39,9 @@ export default defineConfig({
     sidebar,
     i18n,
     footer,
-    lastUpdated: true,
+    lastUpdated: {
+      text: '最近更新',
+    },
     editLink: {
       pattern: 'https://github.com/142vip/core-x/edit/main/docs/:path',
       text: '在Github上编辑',

92eaa4c9f5a548d6364f07655002ca8b3bca5504
chufan <mmdapl@163.com>
2024-08-09 20:28:44 +0800

    feat(@142vip/fairy-cli): 移除`cnpm`模块，基于api实现，支持npm包同步到cnpm平台上

--- packages/fairy-cli/src/commands/sync.ts
+++ packages/fairy-cli/src/commands/sync.ts
@@ -1,5 +1,6 @@
-import { execShell } from '@142vip/common'
+import process from 'node:process'
 import type { Command } from 'commander'
+import fetch from 'node-fetch'
 import { CliCommandEnum } from '../shared'
 
 export interface SyncOptions {
@@ -7,14 +8,88 @@ export interface SyncOptions {
 }
 
 /**
+ * cnpm 同步状态
+ */
+enum CNPMPackageState {
+  Waiting = 'waiting',
+  Processing = 'processing',
+  Success = 'success',
+}
+
+/**
+ * 同步包的响应
+ */
+interface RequestSync {
+  ok: boolean
+  logId: string
+}
+
+console.log(`Usage: $ fa sync [moduleName1 moduleName2 ...]
+Options:
+  --sync-publish        sync as publish
+  --no-deps        do not sync dependencies and devDependencies`)
+
+async function requestSync(packageName: string) {
+  const syncUrl = `https://registry.npmmirror.com/${packageName}/sync`
+
+  const response = await fetch(syncUrl, { method: 'PUT' })
+
+  // if (response.status === 404 || !response.ok) {
+  //
+  // }
+
+  const { ok, logId } = await response.json() as RequestSync
+
+  if (!ok) {
+    console.log('requestSync--json', await response.json())
+    process.exit(1)
+  }
+  setTimeout(async () => {
+    const logUrl = await getPackageSyncState(packageName, logId)
+    await getPackageSyncLog(logUrl)
+  }, 2000)
+}
+
+interface SyncState {
+  ok: boolean
+  id: string
+  type: string
+  state: CNPMPackageState
+  logUrl: string
+}
+
+async function getPackageSyncState(packageName: string, logId: string) {
+  const stateUrl = `https://registry.npmmirror.com/-/package/${packageName}/syncs/${logId}`
+  const response = await fetch(stateUrl)
+  const stateRes = await response.json() as SyncState
+
+  // 正常的请求
+  if (stateRes.ok && stateRes.state === CNPMPackageState.Success) {
+    return stateRes.logUrl
+  }
+  console.log('getPackageSyncState-->err', stateRes)
+  process.exit(1)
+}
+
+async function getPackageSyncLog(logUrl: string) {
+  const response = await fetch(logUrl)
+
+  const syncLog = await response.text()
+  // console.log('getPackageSyncLog', syncLog)
+  console.log(syncLog.toString())
+}
+
+/**
  * 同步到cnpm仓库
  * @param packageNames
  */
 async function execSync(packageNames: string[]) {
-  await execShell(packageNames.map(packageName => ({
-    command: `npx cpnm sync ${packageName}`,
-    description: `同步模块${packageName}到cnpm仓库`,
-  })))
+  for (const packageName of packageNames) {
+    setTimeout(async () => {
+      console.log(`---------模块：${packageName} 开始同步 ------- \n`)
+      await requestSync(packageName)
+    }, 1000)
+  }
 }
 
 /**

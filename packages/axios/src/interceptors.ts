462d60e19228ef0606d6b1a57a4b9b5105756b17
chufan <mmdapl@163.com>
2024-08-12 11:40:00 +0800

    feat(@142vip/axios): 模块初始化，支持`method`等枚举导出

---
+++ packages/axios/src/interceptors.ts
@@ -0,0 +1,28 @@
+// 拦截器类型
+export enum Interceptor_Type {
+  Request = 'request',
+  Response = 'response',
+}
+
+/**
+ * 请求拦截器
+ */
+export function requestInterceptor() {
+
+}
+
+/**
+ * 响应拦截器
+ */
+
+export function responseInterceptor() {
+
+}
+
+export function useInterceptor() {
+
+}
+
+export function clearInterceptor() {
+
+}

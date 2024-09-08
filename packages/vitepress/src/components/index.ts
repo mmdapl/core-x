f57aca76b22074c34624c65f13fdf49a476b586f
chufan <mmdapl@163.com>
2024-09-08 11:37:47 +0800

    feat(@142vip/vitepress): 引入`element-plus`、`vue`等模块，封装`ProjectIntroduce`、`VipBackUp`等组件，支持表格展示页面

--- packages/vitepress/src/components/index.ts
+++ packages/vitepress/src/components/index.ts
@@ -1,5 +1,9 @@
 import VipContactAuthor from './VipContactAuthor.vue'
+import VipBackUp from './VipBackUp.vue'
+import ProjectIntroduce from './ProjectIntroduce.vue'
 
 export {
   VipContactAuthor,
+  VipBackUp,
+  ProjectIntroduce,
 }
--- packages/vitepress/src/core/config.ts
+++ packages/vitepress/src/core/config.ts
@@ -1,3 +1,5 @@
+import type { DefaultTheme } from 'vitepress/types/default-theme'
+
 export const i18n = {
   search: '搜索',
   menu: '菜单',
@@ -32,6 +34,50 @@ export const i18n = {
   ariaSidebarNav: '侧边栏导航',
 }
 
+export const zhSearch: DefaultTheme.AlgoliaSearchOptions['locales'] = {
+  root: {
+    placeholder: '搜索文档',
+    translations: {
+      button: {
+        buttonText: '搜索文档',
+        buttonAriaLabel: '搜索文档',
+      },
+      modal: {
+        searchBox: {
+          resetButtonTitle: '清除查询条件',
+          resetButtonAriaLabel: '清除查询条件',
+          cancelButtonText: '取消',
+          cancelButtonAriaLabel: '取消',
+        },
+        startScreen: {
+          recentSearchesTitle: '搜索历史',
+          noRecentSearchesText: '没有搜索历史',
+          saveRecentSearchButtonTitle: '保存至搜索历史',
+          removeRecentSearchButtonTitle: '从搜索历史中移除',
+          favoriteSearchesTitle: '收藏',
+          removeFavoriteSearchButtonTitle: '从收藏中移除',
+        },
+        errorScreen: {
+          titleText: '无法获取结果',
+          helpText: '你可能需要检查你的网络连接',
+        },
+        footer: {
+          selectText: '选择',
+          navigateText: '切换',
+          closeText: '关闭',
+          searchByText: '搜索供应商',
+        },
+        noResultsScreen: {
+          noResultsText: '无法找到相关结果',
+          suggestedQueryText: '你可以尝试查询',
+          reportMissingResultsText: '你认为该查询应该有结果？',
+          reportMissingResultsLinkText: '点击反馈',
+        },
+      },
+    },
+  },
+}
+
 // 默认主题配置
 export const defaultThemeConfig = {
   lang: 'zh-CN',
@@ -54,7 +100,10 @@ export const defaultThemeConfig = {
     },
     // 一些链接
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/142vip/core-x' },
+      {
+        icon: 'github',
+        link: 'https://github.com/142vip/core-x',
+      },
       { icon: 'npm', link: 'https://www.npmjs.com/search?q=%40142vip' },
     ],
     search: {

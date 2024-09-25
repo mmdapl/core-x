81f3e3e3b90b28b1c999d42d3fd92f92add6b808
chufan <mmdapl@163.com>
2024-09-25 17:54:07 +0800

    feat(@142vip/fairy-cli): 拓展`changelog`命令，支持`cli`工具快速执行`npx changelog`相关命令

--- packages/fairy-cli/src/commands/changelog.ts
+++ packages/fairy-cli/src/commands/changelog.ts
@@ -1,34 +1,22 @@
 import type { Command } from 'commander'
+import { commandStandardExecutor } from '@142vip/utils'
 import { CliCommandEnum } from '../shared'
 
 interface ChangelogOptions {
-  packageName?: string
-  packageDir?: string
-  output: string
+  dry?: boolean
 }
 
-function generateChangelog(args: ChangelogOptions) {
-  // 指定目录下的模块，生成changelog文档
-  if (args.packageDir != null && args.packageName != null) {
-    console.log(args.output)
-  }
-  // 生成根目录下的changelog文档
-  else {
-    // execCommand(`npx changelog --output ${args.output}`)
-  }
+async function generateChangelog(args: ChangelogOptions) {
+  await commandStandardExecutor(`npx changelog ${args.dry ? '--dry' : ''}`)
 }
 
 export async function changelogMain(program: Command) {
   program
     .command(CliCommandEnum.CHANGELOG)
-    .description('生成CHANGELOG日志文档')
-    .option('--package', 'registry address')
-    .option('--package-dir', 'Monorepo包存在的相对路径，默认：packages', 'packages')
-    .option('--output', '日志文档保存的文件名，默认：CHANGELOG.md', 'CHANGELOG.md')
-    // .option('--execute', 'registry address')
-    .action((args: ChangelogOptions) => {
+    .description('快速使用@142vip/changelog模块')
+    .option('--dry', '试运行，生成`CHANGELOG`文档', false)
+    .action(async (args: ChangelogOptions) => {
       // 参考 @142vip/changelog模块
-      console.log(CliCommandEnum.CHANGELOG, args)
-      generateChangelog(args)
+      await generateChangelog(args)
     })
 }

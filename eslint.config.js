d3f83d7e31474cf78fc1022cfdbd31ff39ac7005
chufan <mmdapl@163.com>
2024-09-26 17:05:33 +0800

    feat: 移除Eslint相关冗余依赖，配置使用`@142vip/eslint-config`模块

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,10 @@
-import antFu from '@antfu/eslint-config'
+import { defineVipEslintConfig } from '@142vip/eslint-config'
 
-// 参考：https://github.com/antfu/eslint-config
-export default antFu({
+export default defineVipEslintConfig({
   ignores: [
-    '**/node_modules/**',
-    '**/dist/**',
-    '**/.nuxt/**',
-    '**/typings/**/',
-    '**/.output/**',
-    // 基于release脚本生成
     '**/CHANGELOG.md',
   ],
-  typescript: true,
-  vue: true,
-  jsonc: true,
-  yaml: true,
-  formatters: {
-    css: true,
-    html: true,
-    markdown: true,
-  },
   rules: {
-    'no-console': 'warn',
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'CallExpression[callee.object.name=\'console\'][callee.property.name!=/^(log|warn|error|info|trace)$/]',
-        message: 'Unexpected property on console object was called',
-      },
-    ],
     // 用于在模块构建后基于dist导出时找不到文件，忽略校验
     'antfu/no-import-dist': 1,
   },

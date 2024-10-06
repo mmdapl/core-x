a8894aaa9bd5028063158ac8e6ec6cda91ebf677
chufan <mmdapl@163.com>
2024-10-06 18:13:13 +0800

    feat(@142vip/vuepress): 增加开源博客站点的`header`配置

--- packages/vuepress/src/core/headers.ts
+++ packages/vuepress/src/core/headers.ts
@@ -1,7 +1,7 @@
 import type { HeadConfig } from '../types'
 
 /**
- * 408CSFamily
+ * 示例配置
  */
 export const exampleHeaders: HeadConfig[] = [
   [
@@ -33,9 +33,26 @@ export const exampleHeaders: HeadConfig[] = [
 export const FamilyHeaders: HeadConfig[] = [
   [
     'link',
-    { rel: 'icon', href: 'fight_favicon.ico' },
+    { rel: 'icon', href: '/408_favicon.ico' },
   ],
   // vercel统计 相关配置
+
+  [
+    'script',
+    { type: 'text/javascript', src: '/_vercel/insights/script.js' },
+  ],
+  // 百度统计
+  [
+    'script',
+    {},
+    `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?3515cc46ae60747b778140f0e5e22dfe";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();`,
+  ],
 ]
 
 /**
@@ -47,4 +64,20 @@ export const JSCHeaders: HeadConfig[] = [
     { rel: 'icon', href: 'fight_favicon.ico' },
   ],
   // vercel统计 相关配置
+  [
+    'script',
+    { type: 'text/javascript', src: '/_vercel/insights/script.js' },
+  ],
+  // 百度统计
+  [
+    'script',
+    {},
+    `var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?613c9d7af9e1c9a7f9eef6a55aa2399d";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();`,
+  ],
 ]

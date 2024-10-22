6dcb9a5c7f20012e7f41105d8bc669fc879b590c
chufan <mmdapl@163.com>
2024-10-22 16:06:14 +0800

    feat(@142vip/vitepress): 优化`defineVipExtendsTheme`参数类型，继承默认的`Theme`类型

--- packages/vitepress/src/theme/index.ts
+++ packages/vitepress/src/theme/index.ts
@@ -1,27 +1,46 @@
 import DefaultTheme from 'vitepress/theme'
 import type { EnhanceAppContext } from 'vitepress/dist/client'
 import ElementPlus from 'element-plus'
+import type { Component } from 'vue'
 import { h } from 'vue'
+import type { Awaitable } from 'vitepress/types/shared'
 import { VipBackTop } from '../components'
 
 /**
- * 集成vitepress的默认主题，自定义拓展
+ * vitepress默认主题
+ * - 参考：https://vitepress.dev/zh/guide/custom-theme
  */
-export default function defineVipExtendsTheme(theme?: any) {
+interface Theme {
+  Layout?: Component
+  enhanceApp?: (ctx: EnhanceAppContext) => Awaitable<void>
+  extends?: Theme
+  /**
+   * @deprecated can be replaced by wrapping layout component
+   */
+  setup?: () => void
+  /**
+   * @deprecated Render not found page by checking `useData().page.value.isNotFound` in Layout instead.
+   */
+  NotFound?: Component
+}
+
+/**
+ * 集成vitepress的默认主题，自定义拓展
+ * - 参考：https://vitepress.dev/guide/extending-default-theme#layout-slots
+ */
+export default function defineVipExtendsTheme(theme?: Theme) {
   return {
     extends: DefaultTheme,
     Layout: () => {
       return h(DefaultTheme.Layout, null, {
-        // https://vitepress.dev/guide/extending-default-theme#layout-slots
+        // 增加返回顶部组件
         'doc-bottom': () => h(VipBackTop),
       })
     },
-    // todo 查看这里的app、router、siteData的作用
     enhanceApp: ({ app }: EnhanceAppContext) => {
       app.use(ElementPlus)
     },
-
-    // 自定义拓展
+    // 自定义拓展，配置覆盖
     ...theme != null ? theme : {},
   }
 }

c0db53577efdcb2d0c621aad18e592ef6eeb6b8a
chufan <mmdapl@163.com>
2025-01-05 16:58:35 +0800

    feat(@142vip/egg-grpc-client): 调整插件加载模式，`grpc-client`结构初始化

--- packages/egg-grpc-client/app.js
+++ packages/egg-grpc-client/app.js
@@ -1,16 +1,13 @@
-const { registerPlugin, RegisterEggPluginName } = require('@142vip/egg')
+const { RegisterEggPluginName, EggPluginBoot } = require('@142vip/egg')
 const { createEggGrpcClientInstance } = require('./core/grpc-client')
 
-class EggGrpcClientAppBoot {
+class EggGrpcClientAppBoot extends EggPluginBoot {
   constructor(agent) {
-    this.agent = agent
-  }
-
-  // 所有文件已加载，此时可以启动插件。
-  async didLoad() {
-    if (this.agent.config[RegisterEggPluginName.EGG_GRPC_CLIENT]) {
-      registerPlugin(RegisterEggPluginName.EGG_GRPC_CLIENT, this.agent, createEggGrpcClientInstance())
-    }
+    super({
+      pluginName: RegisterEggPluginName.EGG_GRPC_CLIENT,
+      appOrAgent: agent,
+      createEggPluginInstance: createEggGrpcClientInstance,
+    })
   }
 }
 
--- packages/egg-grpc-client/core/grpc-client.js
+++ packages/egg-grpc-client/core/grpc-client.js
@@ -1,8 +1,7 @@
 const { VipEggPluginLogger } = require('@142vip/egg')
-const { name: pkgName } = require('../package.json')
 
-function createEggGrpcClientInstance(config, app) {
-  const pluginLogger = VipEggPluginLogger.getInstance(pkgName, app)
+function createEggGrpcClientInstance(pluginConfig, app) {
+  const pluginLogger = VipEggPluginLogger.getInstance(pluginConfig, app)
 
   pluginLogger.info('plugin init')
 }

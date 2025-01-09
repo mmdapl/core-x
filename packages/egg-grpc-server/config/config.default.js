364e451681d66cfcf9c0a037e8783c8656a8a9e4
chufan <mmdapl@163.com>
2025-01-09 20:57:29 +0800

    feat(@142vip/egg-grpc-server): 插件模式支持grpc服务端

--- packages/egg-grpc-server/config/config.default.js
+++ packages/egg-grpc-server/config/config.default.js
@@ -4,10 +4,24 @@ const { name: pkgName } = require('../package.json')
 module.exports = {
   grpcServer: defaultPluginConfig(pkgName, {
     default: {
-
+      loaderOptions: {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      },
     },
     client: {
-
+      connectUri: '127.0.0.1:50051',
+      protoPaths: ['./proto/helloworld.proto'],
+      loaderOptions: {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      },
     },
   }),
 }
--- packages/egg-grpc-server/core/grpc-server.js
+++ packages/egg-grpc-server/core/grpc-server.js
@@ -1,10 +1,26 @@
 const { VipEggPluginLogger } = require('@142vip/egg')
+const { GrpcServer, ProtoLoader } = require('@142vip/grpc')
 
-function createEggGrpcServerInstance(pluginConfig, app) {
+async function createEggGrpcServerInstance(pluginConfig, app) {
   const pluginLogger = VipEggPluginLogger.getInstance(pluginConfig, app)
+
+  const grpcServer = GrpcServer.getInstance()
+  const { connectUri, protoPaths, loaderOptions } = pluginConfig
   try {
     pluginLogger.info(`GrpcServer create success , the name is ${pluginConfig.name}`)
-    return {}
+
+    for (const protoPath of protoPaths) {
+      const protoLoader = ProtoLoader.getInstance(protoPath, loaderOptions)
+      const serviceClassDefinition = protoLoader.getServiceClassDefinition()
+
+      // todo
+      grpcServer.addService(serviceClassDefinition, pluginConfig.implementation)
+    }
+
+    // 服务监听
+    await grpcServer.listen(connectUri)
+
+    return grpcServer
   }
   catch (e) {
     pluginLogger.error(`GrpcServer create failed ，check egg-grpc-server config carefully ${e.message}`)

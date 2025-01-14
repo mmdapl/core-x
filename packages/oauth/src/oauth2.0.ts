43fdad44c21bfd27fa300414092a64dafb8b56f8
chufan <mmdapl@163.com>
2025-01-14 09:57:26 +0800

    feat(@142vip/oauth2.0): 增加`Oauth2.0`协议的`StandardOauthV2`类

---
+++ packages/oauth/src/oauth2.0.ts
@@ -0,0 +1,32 @@
+/**
+ * 标准 Oauth2.0
+ */
+export class StandardOauthV2 {
+  /**
+   * 登录授权
+   */
+  public async authorize() {
+
+  }
+
+  /**
+   * 获取令牌
+   */
+  public async getToken() {
+
+  }
+
+  /**
+   * 认证
+   */
+  public async authenticate() {
+
+  }
+
+  /**
+   * 刷新令牌
+   */
+  public async refreshToken() {
+
+  }
+}

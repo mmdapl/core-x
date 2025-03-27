0cea4bbcd62fd08785b6ba57e81e88a1391fa887
142vip.cn <2237221210@qq.com>
2025-03-28 02:10:50 +0800

    feat(@142vip/utils): 新增`createSemver`创建实例，支持`originImportSemVer`原生导入的`semver`对象 (#421)

--- packages/utils/src/pkgs/semver.ts
+++ packages/utils/src/pkgs/semver.ts
@@ -1,15 +1,24 @@
-import type { ReleaseType } from 'semver'
-import {
+import type { RangeOptions, ReleaseType } from 'semver'
+import originImportSemVer, {
   clean,
   compare,
   eq,
   gt,
   inc,
   lt,
+  parse,
   prerelease,
   satisfies,
+  SemVer,
   valid,
 } from 'semver'
+
+/**
+ * 支持原生创建Semver实例
+ */
+function createSemver(version: string | SemVer, optionsOrLoose?: boolean | RangeOptions): SemVer {
+  return new SemVer(version, optionsOrLoose)
+}
 
 /**
  * 参考：https://www.npmjs.com/package/semver
@@ -22,8 +31,11 @@ export const VipSemver = {
   lt,
   eq,
   inc,
+  parse,
   compare,
   prerelease,
+  createSemver,
+  originImportSemVer,
 }
 
 export type VipSemverReleaseType = ReleaseType

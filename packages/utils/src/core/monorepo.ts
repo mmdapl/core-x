ecb6fc2ab45227762688981aad0aa1167b54489e
142vip.cn <2237221210@qq.com>
2025-04-13 16:26:16 +0800

    feat(@142vip/utils): 增加`VipMonorepo`工具，支持`getPackageJSONPathList`函数 (#458)

---
+++ packages/utils/src/core/monorepo.ts
@@ -0,0 +1,36 @@
+import fsSync from 'node:fs'
+import yaml from 'js-yaml'
+import { VipNodeJS } from './nodejs'
+
+/**
+ * 获取monorepo下所有包的package.json，返回所有包的路径列表
+ */
+async function getPackageJSONPathList() {
+  const pnpmYamlFileName = 'pnpm-workspace.yaml'
+  const pnpmWorkspace = VipNodeJS.existPath(pnpmYamlFileName)
+  const packageJSONList: string[] = []
+
+  // 存在pnpm monorepo
+  if (pnpmWorkspace) {
+    // read pnpm-workspace.yaml
+    const pnpmWorkspace = VipNodeJS.readFileToStrByUTF8(pnpmYamlFileName)
+    // parse yaml
+    const workspaces = yaml.load(pnpmWorkspace) as { packages: string[] }
+    // append package.json to each workspace string
+    const workspacesWithPackageJson = workspaces.packages.map(workspace => `${workspace}/package.json`)
+
+    // start with ! or already in files should be excluded
+    packageJSONList.concat(workspacesWithPackageJson.filter(workspace => !workspace.startsWith('!')))
+  }
+
+  // 如果根目录下的package.json存在，则返回根目录下的package.json
+  if (fsSync.existsSync('package.json')) {
+    packageJSONList.push('package.json')
+  }
+
+  return packageJSONList
+}
+
+export const VipMonorepo = {
+  getPackageJSONPathList,
+}

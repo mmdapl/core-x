330376c44568ef8e3d100bdfa7f30ea34a093080
142vip.cn <2237221210@qq.com>
2025-04-21 17:20:48 +0800

    feat(@142vip/utils): 工具增加`initCommand`封装，提供可配置命令初始化 (#492)

--- packages/utils/src/pkgs/commander.ts
+++ packages/utils/src/pkgs/commander.ts
@@ -1,5 +1,18 @@
 import { Command } from 'commander'
 
+export interface VipCommanderDetailOptions {
+  command: string
+  aliases: string[]
+  summary: string
+  description: string
+}
+
+export interface VipCommanderOptions {
+  dryRun?: boolean
+  trace?: boolean
+  vip?: boolean
+  help?: boolean
+}
 /**
  * 终端交互
  * 参考：https://www.npmjs.com/package/commander
@@ -8,15 +21,57 @@ export class VipCommander extends Command {
   constructor(name: string, version: string, description?: string) {
     super(name)
     // 查看版本
-    this.version(version, '-v --version', 'VipCommander Version By @142vip')
+    this.version(version, '-v,--version', 'VipCommander Version By @142vip')
 
     // 添加描述
     if (description != null) {
       this.description(description)
     }
 
+    /**
+     * 增加默认的一些参数
+     */
+    // this
+    //   .option('--dry-run', '试运行', false)
+    //   .option('--vip', '@142vip组织专用功能', false)
+    //   .option('--logger', '开启日志追踪模式', false)
+
     // 对命令增加help方法
-    this.helpCommand(true)
+    // this.helpCommand(true)
+  }
+
+  /**
+   * 对命令初始化，增加aliases，summary，description等信息
+   */
+  public initCommand(options: VipCommanderDetailOptions, {
+    dryRun = true,
+    trace = true,
+    vip = false,
+    help = false,
+  }: VipCommanderOptions): VipCommander {
+    const vipCommander = this.command(options.command, options.description)
+      .aliases(options.aliases)
+      .summary(options.summary)
+      .description(options.description)
+
+    if (trace) {
+      vipCommander.option('--trace', '开启日志追踪模式', false)
+    }
+
+    if (dryRun) {
+      vipCommander.option('--dry-run', '试运行', false)
+    }
+
+    if (vip) {
+      vipCommander.option('--vip', '@142vip组织专用功能', false)
+    }
+
+    // 对命令增加help方法
+    if (help) {
+      vipCommander.helpCommand(true)
+    }
+
+    return vipCommander
   }
 
   // 对description、summary方法重新，增加颜色处理

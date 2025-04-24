27b376e1d4a71a584b41ea36295ec6621d60cd67
chufan <mmdapl@163.com>
2025-04-24 16:35:43 +0800

    feat(@142vip/utils): 优化`VipCommander`的命令、参数初始化逻辑

--- packages/utils/src/pkgs/commander.ts
+++ packages/utils/src/pkgs/commander.ts
@@ -48,38 +48,25 @@ export class VipCommander extends Command {
   }
 
   /**
+   * 初始化，不包括命令
+   */
+  public init(options: Pick<VipCommanderDetailOptions, 'summary' | 'description'>, args: VipCommanderOptions = {}): Command {
+    const vipCommander = this.summary(options.summary)
+      .description(options.description)
+    return this.initOptions(vipCommander, args)
+  }
+
+  /**
    * 对命令初始化，增加aliases，summary，description等信息
    * - 增加默认的一些参数
    */
-  public initCommand(options: VipCommanderDetailOptions, {
-    dryRun = true,
-    trace = true,
-    vip = false,
-    help = false,
-  }: VipCommanderOptions = {}): Command {
+  public initCommand(options: VipCommanderDetailOptions, args: VipCommanderOptions = {}): Command {
     const vipCommander = this.command(options.command)
       .aliases(options.aliases)
       .summary(options.summary)
       .description(options.description)
 
-    if (trace) {
-      vipCommander.option('--trace', '开启日志追踪模式', false)
-    }
-
-    if (dryRun) {
-      vipCommander.option('--dry-run', '试运行', false)
-    }
-
-    if (vip) {
-      vipCommander.option('--vip', '@142vip组织专用功能', false)
-    }
-
-    // 对命令增加help方法
-    if (help) {
-      vipCommander.helpCommand(true)
-    }
-
-    return vipCommander
+    return this.initOptions(vipCommander, args)
   }
 
   // 对description、summary方法重新，增加颜色处理
@@ -90,4 +77,27 @@ export class VipCommander extends Command {
   // public summaryByColor(str: string): this {
   //   return super.summary(str)
   // }
+
+  /**
+   * 初始化参数
+   */
+  private initOptions(program: Command, { dryRun = true, trace = true, vip = false, help = false }: VipCommanderOptions) {
+    if (trace) {
+      program.option('--trace', '开启日志追踪模式', false)
+    }
+
+    if (dryRun) {
+      program.option('--dry-run', '试运行', false)
+    }
+
+    if (vip) {
+      program.option('--vip', '@142vip组织专用功能', false)
+    }
+
+    // 对命令增加help方法
+    if (help) {
+      program.helpCommand(true)
+    }
+    return program
+  }
 }

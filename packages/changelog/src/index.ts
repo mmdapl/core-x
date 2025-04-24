fe60c597a5f92bdf5117ce94631612890bb91f95
142vip.cn <2237221210@qq.com>
2025-04-24 16:46:29 +0800

    refactor(@142vip/changelog): 重构`changelog`核心逻辑，调整模块的整体结构，支持更多API方法 (#501)

--- packages/changelog/src/index.ts
+++ packages/changelog/src/index.ts
@@ -1,27 +1,3 @@
-import type { ChangelogGenerateOptions } from './changelog.interface'
-import { VipLodash } from '@142vip/utils/src'
-import {
-  changelogGenerate,
-  changelogUpdate,
-  sendGithubRelease,
-} from './changelog'
-import { ChangelogDefaultConfig } from './config'
-
-export * from './changelog.interface'
-
-/**
- * 工具函数导出
- */
-export const ChangelogCli = {
-  changelogGenerate,
-  changelogUpdate,
-  sendGithubRelease,
-}
-
-/**
- * 定义配置文件
- * - 合并默认配置
- */
-export function defineChangelogDefaultConfig(config: ChangelogGenerateOptions): ChangelogGenerateOptions {
-  return VipLodash.merge({}, ChangelogDefaultConfig, config)
-}
+export * from './core'
+export * from './enums'
+export * from './shared'

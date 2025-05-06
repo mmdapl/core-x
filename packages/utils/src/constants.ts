1e56875874f2a987089b3d48bcf3c39c8009dc2c
chufan <mmdapl@163.com>
2025-05-06 16:22:50 +0800

    feat(@142vip/utils): 拓展`OPEN_SOURCE_ADDRESS`枚举

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -11,6 +11,10 @@ export enum OPEN_SOURCE_ADDRESS {
   GITHUB_REPO_CORE_X = 'https://github.com/142vip/core-x',
   GITHUB_REPO_OAUTH = 'https://github.com/142vip/142vip-oauth',
 
+  GITEE_REPO_408 = 'https://gitee.com/chufan443/408CSFamily',
+  GITEE_REPO_JSC = 'https://gitee.com/chufan443/JavaScriptCollection',
+  GITEE_REPO_CORE_X = 'https://gitee.com/chufan443/core-x',
+
   /**
    * 142vip 仓库地址
    * - 格式：`${VipDockerAddress}/项目代号:${pkg.name}-${pkg.version}`
@@ -22,10 +26,17 @@ export enum OPEN_SOURCE_ADDRESS {
   GITHUB_ORGANIZATION_NAME = '142vip',
 
   // 账号主页
-  HOME_PAGE_VIP = 'https://github.com/142vip',
-  HOME_PAGE_MMDAPL = 'https://github.com/mmdapl',
-  HOME_PAGE_CHU_FAN = 'https://github.com/chufan443',
-  HOME_PAGE_LIR0015 = 'https://github.com/lir0015',
+  HOME_PAGE_GITHUB_VIP = 'https://github.com/142vip',
+  HOME_PAGE_GITHUB_MMDAPL = 'https://github.com/mmdapl',
+  HOME_PAGE_GITHUB_CHU_FAN = 'https://github.com/chufan443',
+  HOME_PAGE_GITHUB_LIR0015 = 'https://github.com/lir0015',
+
+  HOME_PAGE_GITEE_MMDAPL = 'https://gitee.com/mmdapl',
+  HOME_PAGE_GITEE_VIP = 'https://gitee.com/chufan443',
+  HOME_PAGE_NPM_MMDAPL = 'https://www.npmjs.com/~mmdapl',
+
+  // 域名
+  HOME_PAGE_DOMAIN_VIP = 'https://142vip.cn',
 
   // 证书
   LICENCE_GITHUB = 'https://github.com/142vip/LICENSE',
@@ -49,11 +60,11 @@ export interface VipAuthorInfo {
 export const OPEN_SOURCE_AUTHOR: VipAuthorInfo = {
   name: '微信公众号：储凡',
   email: 'fairy_vip@2925.com',
-  url: 'https://github.com/142vip',
-  homePage: 'https://142vip.cn',
-  github: 'https://github.com/mmdapl',
-  githubVip: 'https://github.com/142vip',
-  gitee: 'https://gitee.com/chufan443',
+  url: OPEN_SOURCE_ADDRESS.HOME_PAGE_GITHUB_VIP,
+  homePage: OPEN_SOURCE_ADDRESS.HOME_PAGE_DOMAIN_VIP,
+  github: OPEN_SOURCE_ADDRESS.HOME_PAGE_GITEE_MMDAPL,
+  githubVip: OPEN_SOURCE_ADDRESS.HOME_PAGE_GITEE_VIP,
+  gitee: OPEN_SOURCE_ADDRESS.HOME_PAGE_GITEE_VIP,
 }
 
 /**

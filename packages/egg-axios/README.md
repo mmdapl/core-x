9db5f748ce8f7d0a0b42da50c5a1801c3d3202d5
142vip.cn <2237221210@qq.com>
2025-09-05 10:02:09 +0800

    feat(@142vip/egg-axios): 集成`@142vip/axios`模块，基于`vipAxios`实例请求 (#610)
    * feat(@142vip/egg): 新增`mergeConfig`函数，合并配置
    
    * feat(@142vip/egg-axios): 集成`@142vip/axios`模块，基于`vipAxios`实例请求
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- packages/egg-axios/README.md
+++ packages/egg-axios/README.md
@@ -2,11 +2,88 @@
 
 [![NPM version](https://img.shields.io/npm/v/@142vip/egg-axios?labelColor=0b3d52&color=1da469&label=version)](https://www.npmjs.com/package/@142vip/egg-axios)
 
+`Egg.js`框架下使用`@142vip/axios`的插件，简化`Http`请求配置
+
 ## 安装
 
-```shell
-# 安装
+```bash
+# npm
+npm i @142vip/axios
+# pnpm
 pnpm i @142vip/egg-axios
+```
+
+## 配置
+
+### 单实例
+```js
+// config/config.default.js
+module.exports = {
+  axios: {
+    headers: {
+      common: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    },
+    // default 5 seconds timeout
+    timeout: 5000,
+  }
+}
+```
+
+### 多个实例
+
+```js
+// config/config.default.js
+exports.eggAxios = {
+  clients: {
+    // clientId，可通过 app.mysql.get('clientId') 访问客户端实例
+    axios1: {
+      headers: {
+        common: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      },
+      // default 5 seconds timeout
+      timeout: 5000,
+    },
+    axios2: {
+      headers: {
+        common: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      },
+    },
+  },
+  // 所有数据库的默认配置
+  default: {
+    timeout: 5000,
+  },
+}
+```
+
+## 使用
+
+### 单实例
+```js
+// app/controller/post.js
+class PostController extends Controller {
+  async list() {
+    const axios = await this.app.axios
+  }
+}
+```
+
+### 多实例
+
+```js
+// app/controller/post.js
+class PostController extends Controller {
+  async list() {
+    const axios1 = await this.app.axios.get('axios1')
+    const axios2 = await this.app.axios.get('axios2')
+  }
+}
 ```
 
 ## 证书
--- packages/egg-axios/core/axios.js
+++ packages/egg-axios/core/axios.js
@@ -1,10 +1,5 @@
-const {
-  createAxiosInstance,
-  defaultRequestInterceptor,
-  defaultResponseInterceptor,
-} = require('@142vip/axios')
-const { VipEggPluginLogger } = require('@142vip/egg')
-const { VipLodash } = require('@142vip/utils')
+const { createVipAxios, defaultRequestInterceptor, defaultResponseInterceptor } = require('@142vip/axios')
+const { VipEggPluginLogger, mergeConfig } = require('@142vip/egg')
 
 /**
  * 创建egg-axios实例
@@ -23,14 +18,14 @@ function createEggAxiosInstance(pluginConfig, app) {
     timeout: 5000,
   }
   // 合并插件用户自定义配置、axios默认配置
-  const config = VipLodash.merge(defaultConfig, pluginConfig)
+  const config = mergeConfig(defaultConfig, pluginConfig)
 
   pluginLogger.debug(`default configs: ${JSON.stringify(config)}`)
 
-  const axios = createAxiosInstance(config)
+  const vipAxios = createVipAxios(config)
 
   //  添加请求拦截器
-  axios.interceptors.request.use(
+  vipAxios.interceptors.request.use(
     pluginConfig.requestInterceptorsHandler ?? defaultRequestInterceptor,
     (error) => {
       pluginLogger.error(`send request error in interceptors, ${error.message}`)
@@ -39,7 +34,7 @@ function createEggAxiosInstance(pluginConfig, app) {
   )
 
   // 响应拦截器
-  axios.interceptors.response.use(
+  vipAxios.interceptors.response.use(
     pluginConfig.responseInterceptorsHandler ?? defaultResponseInterceptor,
     (error) => {
       pluginLogger.error(`receive response error in interceptors, ${error.message}`)
@@ -47,7 +42,7 @@ function createEggAxiosInstance(pluginConfig, app) {
     },
   )
 
-  return axios
+  return vipAxios
 }
 module.exports = {
   createEggAxiosInstance,

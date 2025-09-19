dcece3dd181be9d11137722d55a74ce077553d84
chufan <mmdapl@163.com>
2025-09-19 17:07:56 +0800

    feat(@142vip/utils): 重写VipConfig，使用`cosmiconfig`替换`c12`模块，处理`c12`模块对cjs语法不支持

--- packages/utils/src/pkgs/config.ts
+++ packages/utils/src/pkgs/config.ts
@@ -1,36 +1,48 @@
-import type { LoadConfigOptions } from 'c12'
+import type { OptionsSync } from 'cosmiconfig/dist/types'
+import { cosmiconfigSync } from 'cosmiconfig'
 import { VipLodash } from './lodash'
-
-/**
- * 加载配置
- * - 本地配置，形如：xxx.config.ts
- * - 包配置，package.json中的xxx字段
- */
-async function loadCliConfig<T>(configName: string, defaultValue: any, c12Options?: LoadConfigOptions): Promise<T> {
-  const { loadConfig } = await import('c12')
-  const { config } = await loadConfig({
-    name: configName,
-    packageJson: true,
-    // c12 loadConfig的其他配置
-    ...c12Options == null ? {} : c12Options,
-  })
-
-  return VipLodash.merge({}, defaultValue, config) as T
-}
-
-/**
- * 合并配置
- * @param cliConfig cli自定义配置
- * @param commanderConfig 用户在cli终端输入的配置
- */
-function mergeCommanderConfig<T>(cliConfig: Partial<T>, commanderConfig: Partial<T>): T {
-  return VipLodash.merge({}, cliConfig, commanderConfig) as T
-}
 
 /**
  * 配置加载
  */
-export const VipConfig = {
-  loadCliConfig,
-  mergeCommanderConfig,
+export class VipConfig {
+  /**
+   * 加载配置
+   * - 本地配置，形如：xxx.config.ts
+   * - 包配置，package.json中的xxx字段
+   */
+  public loadCliConfig<T>(configName: string, defaultValue: any, cosmiconfigOptions?: Partial<OptionsSync>): T {
+    const cliConfig = this.loadConfig(configName, cosmiconfigOptions)
+    if (cliConfig == null) {
+      return defaultValue
+    }
+    return VipLodash.merge({}, defaultValue, cliConfig) as T
+  }
+
+  /**
+   * 加载cli配置
+   * @param configName
+   * @param cosmiconfigOptions
+   */
+  public loadConfig<T>(configName: string, cosmiconfigOptions?: Partial<OptionsSync>): T | undefined {
+    const explorerSync = cosmiconfigSync(configName, cosmiconfigOptions)
+
+    const result = explorerSync.search()
+    if (result == null || result.isEmpty) {
+      return undefined
+    }
+
+    return result.config as T
+  }
+
+  /**
+   * 合并配置
+   * @param cliConfig cli自定义配置
+   * @param commanderConfig 用户在cli终端输入的配置
+   */
+  public mergeCommanderConfig<T>(cliConfig: Partial<T>, commanderConfig: Partial<T>): T {
+    return VipLodash.merge({}, cliConfig, commanderConfig) as T
+  }
 }
+
+export const vipConfig = new VipConfig()

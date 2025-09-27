e3266f11bc4277fb6d4e72107930812cb21fbab9
142vip.cn <2237221210@qq.com>
2025-09-27 18:12:14 +0800

    feat(@142vip/egg-mysql): 基于`VipMyQLPool`类初始化插件，建立连接 (#665)

--- packages/egg-mysql/core/mysql.js
+++ packages/egg-mysql/core/mysql.js
@@ -1,37 +1,39 @@
-const { VipEggPluginLogger } = require('@142vip/egg')
-const mysql = require('mysql2/promise')
-const { createDataBase } = require('./database')
-
-async function creatPool(config) {
-  await createDataBase(config)
-
-  const pool = mysql.createPool({
-    host: config.host,
-    port: config.port,
-    user: config.userName,
-    password: config.password,
-    database: config.database,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // 最大空闲连接数，默认等于 `connectionLimit`
-    idleTimeout: 60000, // 空闲连接超时，以毫秒为单位，默认值为 60000 ms
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-  })
-
-  return pool.promise()
-}
+const { VipEggPluginLogger, VipMySQLPool } = require('@142vip/egg')
 
 /**
  * 创建MySQL实例
  */
 async function createEggMysqlInstance(pluginConfig, app) {
   const pluginLogger = VipEggPluginLogger.getInstance(pluginConfig, app)
+
+  // TODO 断言配置，配置校验
   try {
-    const client = await creatPool(pluginConfig)
+    const vipMysqlPool = await new VipMySQLPool({
+      host: pluginConfig.host,
+      port: pluginConfig.port,
+      user: pluginConfig.userName,
+      password: pluginConfig.password,
+      database: pluginConfig.database,
+      waitForConnections: true,
+      connectionLimit: 10,
+      maxIdle: 10, // 最大空闲连接数，默认等于 `connectionLimit`
+      idleTimeout: 60000, // 空闲连接超时，以毫秒为单位，默认值为 60000 ms
+      queueLimit: 0,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 0,
+    })
+
+    if (pluginConfig.database != null) {
+      // 创建database
+      await vipMysqlPool.createDataBaseName(pluginConfig.database)
+    }
+
+    const pool = await vipMysqlPool.pool
+
     pluginLogger.log(`dataBase create success , the name is ${pluginConfig.database}`)
-    return client
+
+    // 返回连接池
+    return pool
   }
 
   catch (e) {

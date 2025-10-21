8d9d1aa054b8a4140c34531c1bc699077e97f769
142vip.cn <2237221210@qq.com>
2025-10-21 16:09:00 +0800

    feat: 修改`Eslint`配置，关闭`ts/consistent-type-imports`规则 (#708)

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,8 @@ export default defineVipEslintConfig({
   rules: {
     // 用于在模块构建后基于dist导出时找不到文件，忽略校验
     'antfu/no-import-dist': 0,
+    // Nest.js的依赖注入会被识别为type-imports，自动fix会在import添加type，导致注入失败，此项必须关闭。
+    'ts/consistent-type-imports': ['off'],
   },
   settings: {
     node: {

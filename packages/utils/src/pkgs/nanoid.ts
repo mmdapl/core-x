caa6536bf73c8784658ca70b3d47681b5de48ccb
142vip.cn <2237221210@qq.com>
2025-11-06 19:06:19 +0800

    refactor(@142vip/utils): 优化`VipNanoId`类核心逻辑 (#733)

--- packages/utils/src/pkgs/nanoid.ts
+++ packages/utils/src/pkgs/nanoid.ts
@@ -13,28 +13,47 @@ export enum Alphabet {
   ONLY_CHAR = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
   COMPLEX = '-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
 }
-/**
- * 获取NanoId对象
- * @param alphabet
- */
-function getNanoId(alphabet?: string): (size?: number) => string {
-  return customAlphabet(alphabet ?? Alphabet.DEFAULT)
+
+export class VipNanoId {
+  /**
+   * 获取NanoId对象，默认字符集为 Alphabet.DEFAULT，只有小写字母和数字
+   * @param alphabet
+   */
+  public getNanoId(alphabet?: string): (size?: number) => string {
+    return customAlphabet(alphabet ?? Alphabet.DEFAULT)
+  }
+
+  /**
+   * 获取随机字符串，默认长度21
+   * - 按照制定规则生成默认字符串
+   */
+  public getRandomId(size?: number): string {
+    return nanoid(size)
+  }
+
+  /**
+   * 获取纯数字的随机字符串
+   * @param size
+   */
+  public getRandomNumberId(size?: number): string {
+    return this.getNanoId(Alphabet.ONLY_NUMBER)(size)
+  }
+
+  /**
+   * 获取纯字母的随机字符串，包含小写字母和大写字母
+   * @param size
+   */
+  public getRandomCharId(size?: number): string {
+    return this.getNanoId(Alphabet.ONLY_CHAR)(size)
+  }
+
+  /**
+   * 获取复杂的随机字符串，包含数字、小写字母、大写字母和特殊字符
+   * @param size
+   */
+  public getRandomComplexId(size?: number): string {
+    return this.getNanoId(Alphabet.COMPLEX)(size)
+  }
 }
 
-/**
- * 获取随机字符串
- * - 按照制定规则生成默认字符串
- */
-function getRandomId(size?: number): string {
-  return nanoid(size)
-}
-
-export interface IVipNanoId {
-  getRandomId: typeof getRandomId
-  getNanoId: typeof getNanoId
-}
-
-export const VipNanoId: IVipNanoId = {
-  getRandomId,
-  getNanoId,
-}
+export const vipNanoId = new VipNanoId()

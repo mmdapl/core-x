b7c12ee81ebaee374f574e0b00c616459eb3f4cb
142vip.cn <2237221210@qq.com>
2025-11-06 17:41:02 +0800

    refactor(@142vip/utils): 优化`VipDayjs`类核心逻辑，支持`vipDayjs`对象导出 (#732)

--- packages/utils/src/pkgs/dayjs.ts
+++ packages/utils/src/pkgs/dayjs.ts
@@ -1,34 +1,56 @@
 import dayjs from 'dayjs'
 
-/**
- * 时间格式：年-月-日 时:分:秒
- */
-const FormatTemplateStr = 'YYYY-MM-DD HH:mm:ss'
-/**
- * 获取当前时间戳
- */
-function getCurrentTimestamp() {
-  return dayjs().valueOf()
+export class VipDayjs {
+  /**
+   * 时间格式：年-月-日 时:分:秒
+   */
+  private readonly FORMAT_TEMPLATE_STR = 'YYYY-MM-DD HH:mm:ss'
+  /**
+   * 获取当前时间戳。单位：毫秒
+   */
+  public getCurrentTimestamp(): number {
+    return dayjs().valueOf()
+  }
+
+  /**
+   * 获取过期时间戳。单位：毫秒
+   * @param duration 过期时间，默认：1小时
+   */
+  public getExpiredTimestamp(duration = 60 * 60 * 1000): number {
+    return this.getCurrentTimestamp() + duration
+  }
+
+  /**
+   * 是否在当前时间之前
+   * @param date
+   */
+  public isBeforeNow(date: Date): boolean {
+    return dayjs().isBefore(dayjs(date))
+  }
+
+  /**
+   * 是否在当前时间之后
+   * @param date
+   */
+  public isAfterNow(date: Date): boolean {
+    return dayjs().isAfter(dayjs(date))
+  }
+
+  /**
+   * 时间格式化，默认： 年-月-日 时:分:秒
+   */
+  public formatDateToStr(date: Date, template?: string): string {
+    return dayjs(date).format(template ?? this.FORMAT_TEMPLATE_STR)
+  }
+
+  /**
+   * 年月日格式化当前时间
+   * - 格式： 2024-08-09
+   */
+  public formatDateToYMD(): string {
+    return this.formatDateToStr(new Date(), 'YYYY-MM-DD')
+  }
 }
 
-/**
- * 时间格式化，默认： 年-月-日 时:分:秒
- */
-function formatDateToStr(date: Date, template?: string) {
-  return dayjs(date).format(template ?? FormatTemplateStr)
-}
-
-/**
- * 年月日格式化当前时间
- * - 格式： 2024-08-09
- */
-function formatDateToYMD(): string {
-  return formatDateToStr(new Date(), 'YYYY-MM-DD')
-}
-
-export const VipDayjs = {
-  getCurrentTimestamp,
-  FormatTemplateStr,
-  formatDateToStr,
-  formatDateToYMD,
-}
+// 导出
+export const vipDayjs = new VipDayjs()

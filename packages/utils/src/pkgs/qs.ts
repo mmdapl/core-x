862b2faf41f11cc9c9882247c5290309fc082045
chufan <mmdapl@163.com>
2025-11-18 19:38:31 +0800

    fix(@142vip/utils): 增加`VipQs`类和工具方法

--- packages/utils/src/pkgs/qs.ts
+++ packages/utils/src/pkgs/qs.ts
@@ -1,21 +1,20 @@
 import type { BooleanOptional, IParseOptions, IStringifyOptions, ParsedQs } from 'qs'
 import qs from 'qs'
 
-/**
- * 序列化 query string
- */
-function stringify(obj: any, options?: IStringifyOptions<BooleanOptional>): string {
-  return qs.stringify(obj, options)
+export class VipQs {
+  /**
+   * 序列化 query string
+   */
+  public stringify(obj: any, options?: IStringifyOptions<BooleanOptional>): string {
+    return qs.stringify(obj, options)
+  }
+
+  /**
+   * 解析 query string
+   */
+  public parse(str: string, options?: IParseOptions<BooleanOptional> & { decoder?: never | undefined }): ParsedQs {
+    return qs.parse(str, options)
+  }
 }
 
-/**
- * 解析 query string
- */
-function parse(str: string, options?: IParseOptions<BooleanOptional> & { decoder?: never | undefined }): ParsedQs {
-  return qs.parse(str, options)
-}
-
-export const VipQs = {
-  stringify,
-  parse,
-}
+export const vipQs = new VipQs()
